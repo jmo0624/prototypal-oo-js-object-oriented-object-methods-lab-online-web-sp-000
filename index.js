dda48cd8148a48fb8516953b76458ebc7e0fcd39
Josh Morris <jmo0624@gmail.com>
2020-09-12 17:41:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ BoardMember.prototype.releasePressStatement = function() {
 }
 
 BoardMember.prototype.sayHi = function() {
-  console.log(`Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}`)
+  console.log(`Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}`);
 }
