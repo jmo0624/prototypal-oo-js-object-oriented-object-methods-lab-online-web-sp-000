a3e1cd0c27a9473677df6ce3acf29da336258e99
Josh Morris <jmo0624@gmail.com>
2020-09-12 17:39:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function BoardMember(name, homeState, training) {
 BoardMember.prototype.veto = function(){
   console.log(`No, I must disagree`);
 }
+
+BoardMember.prototype.approve = function() {
+  console.log(`You can do that!`);
+}
+
+BoardMember.prototype.doCharity = function() {
+  console.log(`I like to help people`);
+}
