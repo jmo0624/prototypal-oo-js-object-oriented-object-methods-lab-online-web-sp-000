2bf034a75d1a510aa3ee8aed4096c3088de398b6
Josh Morris <jmo0624@gmail.com>
2020-09-12 17:41:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ BoardMember.prototype.approve = function() {
 BoardMember.prototype.doCharity = function() {
   console.log(`I like to help people`);
 }
+
+BoardMember.prototype.releasePressStatement = function() {
+  console.log(`You will see great things from Scuber.`);
+}
+
+BoardMember.prototype.sayHi = function() {
+  console.log(`Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}`)
+}
